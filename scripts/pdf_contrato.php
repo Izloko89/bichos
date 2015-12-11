d20c73426e6b196b585757842ded628082965e4e
Izloko89 <viciz.1089@gmail.com>
2015-12-11 15:10:54 -0600

    tabla

--- scripts/pdf_contrato.php
+++ scripts/pdf_contrato.php
@@ -213,15 +213,15 @@ p {
 <div style="width:100%; padding:5 20px; font-size:12px;">
 	<table cellspacing="0" cellpadding="0" border="1">
 		<tr>
-			<td style="width:500px;">
+			<td style="width:400px;">
 				<strong>Nombre del festejado:</strong> <?php echo $nombreEve ?>
 			</td>
-			<td style="width:500px;">
+			<td style="width:400px;">
 				<strong>Edad que cumple:</strong> <?php echo $edad ?>
 			</td>
 		</tr>
 		<tr>
-			<td style="width:500px;">
+			<td style="width:400px;">
 				<strong>Personaje de la fiesta:</strong> <?php echo $personaje ?>
 			</td>
 		</tr>
